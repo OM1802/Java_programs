6e34b5247ce94011ae7283b572f0c5d6a5a18c70
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-02-03 10:42:53 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,28 @@
+//FOR LOOP
+class forloop{
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.print((i+1)+" ");
+        }
+    }
+}
+//WHILE LOOP
+class whileloop{
+    public static void main(String[] args){
+        int i=0;
+        while(i<10){
+            System.out.print((i+1)+" ");
+            i++;
+        }
+    }
+}
+//DO-WHILE
+class dowhile{
+    public static void main(String[] args){
+        int i=0;
+        do{
+            System.out.print((i+1)+" ");
+            i++;
+        }while(i<10);
+    }
+}
