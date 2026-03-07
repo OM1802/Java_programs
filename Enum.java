242b6ae8eecc4b95117c008b494f1b45e0919867
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-03-07 11:54:39 +0530

    Add files via upload

---
+++ Enum.java
@@ -0,0 +1,15 @@
+enum Day{
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY
+}
+
+class Enum{
+    public static void main(String[] args){
+        Day d=Day.TUESDAY;
+        System.out.print(d);
+
+    }
+}
