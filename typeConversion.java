4d813619808e036b95f9032112c8792fe4570277
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-03-07 10:08:36 +0530

    Add files via upload

---
+++ typeConversion.java
@@ -0,0 +1,9 @@
+class typeConversion{
+    public static void main(String[] args){
+        double a=20.5;
+        int b=(int)a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
