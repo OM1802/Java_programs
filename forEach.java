b0788b28626e7724f16e299fd04b0dd5f3b6165a
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-03-07 10:23:22 +0530

    Add files via upload

---
+++ forEach.java
@@ -0,0 +1,8 @@
+public class forEach{
+    public static void main(String[] args){
+        int[] a={10,20,30,40,50};
+        for(int i: a){
+            System.out.println(i);
+        }
+    }
+}
