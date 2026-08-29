db84d1a8e799ad154df2f00a20a30526cbc27232
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-08-29 15:57:26 +0530

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,24 @@
+public class dataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int population = 1000000;
+        long distance = 9000000000L;
+
+        float temperature = 36.5f;
+        double price = 999.99;
+
+        char grade = 'A';
+
+        boolean passed = true;
+
+        String name = "Om";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
