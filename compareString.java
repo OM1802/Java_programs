1f48e59c242a940bce2c2ad88e2e69327764234f
OM_JAISWAL <omjaiswal1802@gmail.com>
2026-08-31 11:36:29 +0530

    Add files via upload

---
+++ compareString.java
@@ -0,0 +1,10 @@
+class Compare {
+    public static void main(String[] args) {
+
+        String a = new String("HELLO");
+        String b = new String("HELLO");
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+}
